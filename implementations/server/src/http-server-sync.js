8b452cf7de55d8a05f35bad0a255da20c31b43ec
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-08-27 20:51:21 -0300

    fix(implementations): sync server

--- implementations/server/src/http-server-sync.js
+++ implementations/server/src/http-server-sync.js
@@ -3,40 +3,27 @@ import fs from "node:fs";
 import { stat } from "node:fs/promises";
 import { createServer } from "node:http";
 
-import { abortController, PORT, filename, headers } from "./constants.js";
+import { PORT, filename, headers } from "./constants.js";
 
 createServer(async (request, response) => {
-  const items = 0;
-  const maxMemoryUsage = process.memoryUsage().rss;
-
   if (request.method === "OPTIONS") {
     response.writeHead(204, headers);
     response.end();
     return;
   }
 
-  request.once("close", () => {
-    console.log(`connection was closed with ${items} items processed`);
-    console.log(`max memory usage: ${byteSize(maxMemoryUsage)}`);
-    abortController.abort();
-  });
-
-  response.writeHead(200, headers);
-
   try {
     const { size } = await stat(filename);
-    console.log("processing: ", `${byteSize(size)}`);
+    console.log("processing:", byteSize(size));
 
-    fs.readFile(filename, "utf8", (err, data) => {
-      if (err) {
-        response.statusCode = 500;
-        throw err;
-      }
+    response.writeHead(200, headers);
 
-      response.end(data);
-    });
+    const data = await fs.promises.readFile(filename, "utf8");
+    response.end(data);
   } catch (error) {
-    console.log(`Error at server: ${error.message}`);
+    console.error(`Error at server: ${error.message}`);
+    response.statusCode = 500;
+    response.end("Internal Server Error");
   }
 })
   .listen(PORT)
